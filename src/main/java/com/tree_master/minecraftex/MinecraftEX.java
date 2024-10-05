e7f220a625cbfd3c74cbe5a0d8b04e5fc19d12f7
Rose <legoninjafun@gmail.com>
2024-10-05 14:57:27 -0400

    Added some initialization code to the main class.

--- src/main/java/com/tree_master/minecraftex/MinecraftEX.java
+++ src/main/java/com/tree_master/minecraftex/MinecraftEX.java
@@ -3,7 +3,9 @@ package com.tree_master.minecraftex;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -14,18 +16,29 @@ public class MinecraftEX
     public static final String NAME = "MinecraftEX";
     public static final String VERSION = "1.0";
 
+    @SidedProxy(clientSide = "tree_master.minecraftex.proxy.ClientProxy", serverSide = "tree_master.minecraftex.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static MinecraftEX instance;
+
     private static Logger logger;
 
-    @EventHandler
+    @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        proxy.preInit(event);
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        proxy.init(e);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+        proxy.postInit(e);
     }
 }
